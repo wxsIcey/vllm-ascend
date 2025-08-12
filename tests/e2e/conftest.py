fc31fce99572cda75ab092a187fe05565f381160
Icey <1790571317@qq.com>
2025-08-12 11:38:10 +0000

    fix
    Signed-off-by: Icey <1790571317@qq.com>

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -49,7 +49,6 @@ from vllm_ascend.utils import adapt_patch  # noqa E402
 adapt_patch(True)
 adapt_patch(False)
 
-
 from vllm.distributed.parallel_state import (  # noqa E402
     destroy_distributed_environment, destroy_model_parallel)
 
